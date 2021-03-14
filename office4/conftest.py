ef46c11db67967dbf3e9e11d23cc032605526a6a
agpmoita <71726305+agpmoita@users.noreply.github.com>
2021-03-14 20:14:15 +0000

    first draft

---
+++ office4/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from office4.users.models import User
+from office4.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
